a9dd6c0fa642a04084fafaade1bd3519024a7bff
Corey <corey@fortelinea.com>
2022-03-23 14:17:37 -0700

    Add basic swagger docs. Doesn't generate OData controller docs.

--- OData7Swagger/Startup.cs
+++ OData7Swagger/Startup.cs
@@ -27,10 +27,11 @@ namespace OData7Swagger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddControllers();
             services.AddMvc(options => options.EnableEndpointRouting = false);
 
             services.AddOData();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,18 +42,6 @@ namespace OData7Swagger
                 app.UseDeveloperExceptionPage();
             }
 
-            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
-            builder.EntitySet<Customer>("Customers");
-
-            app.UseMvc(routeBuilder =>
-                       {
-                           routeBuilder.Select()
-                                       .Count()
-                                       .Filter()
-                                       .OrderBy();
-                           routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
-                       });
-
             app.UseRouting();
 
             app.UseAuthorization();
@@ -61,6 +50,24 @@ namespace OData7Swagger
             {
                 endpoints.MapControllers();
             });
+
+            app.UseMvc(routeBuilder =>
+                       {
+                           var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+                           builder.EntitySet<Customer>("Customers");
+
+                           routeBuilder.Select()
+                                       .Count()
+                                       .Filter()
+                                       .OrderBy();
+                           routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+                       });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                             {
+                                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "OData 7.5 OpenAPI");
+                             });
         }
     }
 }
